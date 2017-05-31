7f27b366bde4bebd24c2dfabe067b49b2c94ff3b
noppoman <yuki@miketokyo.com>
2017-05-31 19:49:43 +0900

    Use URLSession instead of Prorosum.HTTPClient to avoid ssl verify setup failure

--- Sources/HexavilleAuth/OAuth/OAuth2.swift
+++ Sources/HexavilleAuth/OAuth/OAuth2.swift
@@ -13,6 +13,37 @@ public enum OAuth2Error: Error {
     case invalidAuthrozeURL(String)
 }
 
+#if os(Linux)
+let _urlSessionShared = URLSession(configuration: URLSessionConfiguration(), delegate: nil, delegateQueue: nil)
+    extension URLSession {
+        static var shared: URLSession {
+            return _urlSessionShared
+        }
+    }
+#endif
+
+extension URLSession {
+    func resumeSync(with request: URLRequest) throws -> (HTTPURLResponse, Data) {
+        let chan = Channel<(Error?, (HTTPURLResponse, Data)?)>.make(capacity: 1)
+        
+        let task = self.dataTask(with: request) { data, response, error in
+            if let error = error {
+                try! chan.send((error, nil))
+                return
+            }
+            try! chan.send((nil, (response as! HTTPURLResponse, data!)))
+        }
+        
+        task.resume()
+        
+        let (err, tupple) = try chan.receive()
+        if let error = err {
+            throw error
+        }
+        return (tupple!.0, tupple!.1)
+    }
+}
+
 public class OAuth2 {
     let consumerKey: String
     let consumerSecret: String
@@ -68,23 +99,31 @@ public class OAuth2 {
             "redirect_uri=\(self.callbackURL.absoluteURL()!.absoluteString)"
         ]
         
-        let client = try HTTPClient(url: url)
-        try client.open()
-        let response = try client.request(
-            method: .post,
-            headers: [
-                "Accept": "application/json",
-                "Content-Type": "application/x-www-form-urlencoded"
-            ],
-            body: body.joined(separator: "&").data
-        )
+        var request = URLRequest(url: url)
+        request.httpMethod = "POST"
+        request.addValue("application/json", forHTTPHeaderField: "Accept")
+        request.addValue("application/x-www-form-urlencoded", forHTTPHeaderField: "Content-Type")
+        request.httpBody = body.joined(separator: "&").data
+        
+        let (response, bodyData) = try URLSession.shared.resumeSync(with: request)
         
         guard (200..<300).contains(response.statusCode) else {
+            var headers: Headers = [:]
+            for el in response.allHeaderFields {
+                headers[el.key.description] = "\(el.value)"
+            }
+            
+            let response = Response(
+                status: Response.Status(statusCode: response.statusCode),
+                headers: headers,
+                body: bodyData
+            )
+            
             throw HexavilleAuthError.responseError(response)
         }
         
         do {
-            let bodyDictionary = try JSONSerialization.jsonObject(with: response.body.asData(), options: []) as! [String: Any]
+            let bodyDictionary = try JSONSerialization.jsonObject(with: bodyData, options: []) as! [String: Any]
             return try Credential(withDictionary: bodyDictionary)
         } catch {
             throw error
