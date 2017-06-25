161174a3f0266b2148a52027938133515cb553bd
noppoman <yuki@miketokyo.com>
2017-06-25 19:25:52 +0900

    change usage of AuthenticationMiddleware
    * remove HexavilleAuth.authenticationMiddleware method
    * update useage of AuthenticationMiddleware to app.use(HexavilleAuth.AuthenticationMiddleware())

--- Sources/HexavilleAuth/HexaviileAuth.swift
+++ Sources/HexavilleAuth/HexaviileAuth.swift
@@ -12,34 +12,6 @@ public enum CredentialProviderType {
     case oauth1(OAuth1AuthorizationProvidable)
 }
 
-extension ApplicationContext {
-    public func isAuthenticated() -> Bool {
-        return loginUser != nil
-    }
-    
-    public var loginUser: LoginUser? {
-        get {
-            return memory[AuthenticationMiddleware.sessionKey] as? LoginUser
-        }
-        set {
-            return memory[AuthenticationMiddleware.sessionKey] = newValue
-        }
-    }
-}
-
-public struct AuthenticationMiddleware: Middleware {
-    
-    static var sessionKey = "hexaville.auth.loginUser"
-    
-    public func respond(to request: Request, context: ApplicationContext) throws -> Chainer {
-        if let dict = context.session?[AuthenticationMiddleware.sessionKey] as? [String: Any] {
-            context.loginUser = try LoginUser(fromDictionary: dict)
-        }
-        
-        return .next(request)
-    }
-}
-
 public struct HexavilleAuth {
     var providers: [CredentialProviderType] = []
     
@@ -52,64 +24,19 @@ public struct HexavilleAuth {
     public mutating func add(_ provider: OAuth2AuthorizationProvidable) {
         self.providers.append(.oauth2(provider))
     }
-    
-    public func authenticationMiddleware() -> Middleware {
-        return AuthenticationMiddleware()
+}
+
+extension ApplicationContext {
+    public func isAuthenticated() -> Bool {
+        return loginUser != nil
     }
     
-    public func asRouter() -> Router {
-        let router = Router()
-        for type in providers {
-            switch type {
-            case .oauth1(let provider):
-                router.use(.get, provider.path) { request, context in
-                    let requestToken = try provider.getRequestToken()
-                    context.session?["hexaville.oauth_token_secret"] = requestToken.oauthTokenSecret
-                    context.session?["hexaville.oauth_token"] = requestToken.oauthToken
-                    let location = try provider.createAuthorizeURL(requestToken: requestToken).absoluteString
-                    
-                    return Response(status: .found, headers: ["Location": location])
-                }
-                
-                router.use(.get, provider.oauth.callbackURL.path) { request, context in
-                    guard let secret = context.session?["hexaville.oauth_token_secret"] as? String else {
-                        throw OAuth1Error.accessTokenIsMissingInSession
-                    }
-                    
-                    guard let token = context.session?["hexaville.oauth_token"] as? String else {
-                        throw OAuth1Error.accessTokenIsMissingInSession
-                    }
-                    
-                    let requestToken = RequestToken(
-                        oauthToken: token,
-                        oauthTokenSecret: secret,
-                        oauthCallbackConfirmed: nil
-                    )
-                    
-                    let (cred, user) = try provider.authorize(request: request, requestToken: requestToken)
-                    context.session?[AuthenticationMiddleware.sessionKey] = user.serialize()
-                    return try provider.callback(cred, user, request, context)
-                }
-
-                
-            case .oauth2(let provider):
-                router.use(.get, provider.path) { request, context in
-                    return Response(
-                        status: .found,
-                        headers: [
-                            "Location": try provider.createAuthorizeURL().absoluteString
-                        ]
-                    )
-                }
-                
-                router.use(.get, provider.oauth.callbackURL.path) { request, context in
-                    let (cred, user) = try provider.authorize(request: request)
-                    context.session?[AuthenticationMiddleware.sessionKey] = user.serialize()
-                    return try provider.callback(cred, user, request, context)
-                }
-            }
+    public var loginUser: LoginUser? {
+        get {
+            return memory[HexavilleAuth.AuthenticationMiddleware.sessionKey] as? LoginUser
         }
-        
-        return router
+        set {
+            return memory[HexavilleAuth.AuthenticationMiddleware.sessionKey] = newValue
+        }
     }
 }
